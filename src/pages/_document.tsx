31ea2633398c55a016fdf2668cf651a3443bd946
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-02 22:01:56 -0300

    feat: add stuff on _document page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 /**
- * @component Document - Next.js Document component
+ * @component Document - Next.js Document
  */
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // configuração para utilizar styled-components no Next.js
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -28,5 +29,37 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Lexend:wght@300;500;600&display=swap'
+            rel='stylesheet'
+          />
+
+          {/* <link rel='icon' href='/favicon2.png' type='image/png' /> */}
+
+          <meta
+            name='description'
+            content='DevJobs | The best developer job opportunities, always.'
+          />
+
+          <meta property='og:title' content='DevJobs | Find a new developer job' />
+
+          <meta
+            property='og:description'
+            content='DevJobs | The best developer job opportunities, always.'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
