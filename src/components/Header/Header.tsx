141fe59534ca9af5ecdcffd552226dac8b850a30
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-07 23:54:53 -0300

    feat: link header's logo to root page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import * as S from './Header.styles';
+import Link from 'next/link';
 
 /**
  * @component Header - Website header
@@ -7,7 +8,9 @@ export default function Header() {
   return (
     <S.HeaderContainer>
       <S.HeaderContent>
-        <S.Logo>devjobs</S.Logo>
+        <Link href='/'>
+          <S.Logo>devjobs</S.Logo>
+        </Link>
       </S.HeaderContent>
     </S.HeaderContainer>
   );
