704d9923e9cb6485e8929211d39f86d4f688c183
TiagoDiass <tiago.costadiasss@gmail.com>
2021-12-08 22:53:02 -0300

    feat: add JobDetails page, only with mobile layout for now

--- src/templates/JobDetails/JobDetails.styles.ts
+++ src/templates/JobDetails/JobDetails.styles.ts
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Animations } from 'styles';
 
 export const JobDetailsHeader = styled.header`
   background-color: ${(p) => p.theme.colors.white};
-  padding: 2.5rem 2rem;
+  padding: 2rem;
   border-radius: ${(p) => p.theme.borderRadius};
   box-shadow: ${(p) => p.theme.boxShadow};
   animation: ${Animations.fadeFromLeft} 0.75s ease;
@@ -34,7 +34,7 @@ export const CompanyLogo = styled.div<CompanyLogoParams>`
   top: -2rem;
 `;
 
-export const JobDetailsHeaderContent = styled.div`
+export const JobDetailsHeaderContent = styled.header`
   padding: 1.2rem 0;
   display: flex;
   align-items: center;
@@ -70,4 +70,94 @@ export const JobDetailsHeaderContent = styled.div`
       background-color: rgba(89, 100, 224, 0.2);
     }
   }
+`;
+
+export const JobDetailsContent = styled.section`
+  background-color: ${(p) => p.theme.colors.white};
+  padding: 2.5rem 1.5rem;
+  border-radius: ${(p) => p.theme.borderRadius};
+  box-shadow: ${(p) => p.theme.boxShadow};
+  animation: ${Animations.fadeFromLeft} 0.75s ease;
+`;
+
+export const JobDetailsContentHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+
+  .position {
+    color: ${(p) => p.theme.colors.secondary};
+    font-weight: 900;
+    font-size: 1.4rem;
+  }
+
+  .secondary {
+    color: ${(p) => p.theme.colors.darkGray};
+    font-size: 1.2rem;
+  }
+
+  .location {
+    font-weight: 700;
+    color: ${(p) => p.theme.colors.primary};
+  }
+`;
+
+export const ApplyButton = styled.a`
+  ${({ theme }) => css`
+    display: block;
+    text-align: center;
+    padding: 1rem;
+
+    color: ${theme.colors.white};
+    font-weight: 600;
+    font-size: 1.15rem;
+    text-decoration: none;
+
+    background-color: ${theme.colors.primary};
+    border-radius: ${theme.borderRadius};
+    border-radius: 6px;
+
+    transition: all 0.2s ease;
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(0.95);
+    }
+  `}
+`;
+
+export const Paragraph = styled.p`
+  line-height: 1.6rem;
+  font-size: 1.1rem;
+  color: ${(p) => p.theme.colors.darkGray};
+`;
+
+export const Heading = styled.h3`
+  color: ${(p) => p.theme.colors.secondary};
+  font-weight: 900;
+  font-size: 1.4rem;
+`;
+
+export const List = styled.ul`
+  margin-left: 2rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+
+  li {
+    padding-left: 0.5rem;
+  }
+
+  li::marker {
+    color: ${(p) => p.theme.colors.primary};
+    font-weight: bold;
+  }
+`;
+
+export const JobDetailsFooter = styled.footer`
+  background: ${(p) => p.theme.colors.white};
+  width: 100vw;
+  margin-left: -2rem;
+  padding: 1.5rem 2.5rem;
+  box-shadow: ${(p) => p.theme.boxShadow};
 `;
--- src/templates/JobDetails/JobDetails.tsx
+++ src/templates/JobDetails/JobDetails.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head';
 import * as S from './JobDetails.styles';
 import { Job } from 'types/Job';
-import { Layout } from 'styles/layout-components';
+import { Layout, Spacing } from 'styles/layout-components';
+import { memo } from 'react';
 
 export type JobDetailsProps = {
   job: Job;
 };
 
 export default function JobDetailsTemplate({ job }: JobDetailsProps) {
+  const ApplyButton = memo(() => (
+    <S.ApplyButton href={job.apply} target='_blank' rel='noopener noreferrer'>
+      Apply Now
+    </S.ApplyButton>
+  ));
+
   return (
     <Layout as='main'>
       <Head>
@@ -34,6 +41,68 @@ export default function JobDetailsTemplate({ job }: JobDetailsProps) {
           </a>
         </S.JobDetailsHeaderContent>
       </S.JobDetailsHeader>
+
+      <Spacing space='medium' />
+
+      <S.JobDetailsContent>
+        <S.JobDetailsContentHeader>
+          <p className='secondary'>
+            {job.postedAt} ⚬ {job.contract}
+          </p>
+          <h1 className='position'>{job.position}</h1>
+          <p className='location'>{job.location}</p>
+        </S.JobDetailsContentHeader>
+
+        <Spacing space='large' />
+
+        <ApplyButton />
+
+        <Spacing space='medium' />
+
+        <S.Paragraph>{job.description}</S.Paragraph>
+
+        <Spacing space='large' />
+
+        <S.Heading>Requirements</S.Heading>
+
+        <Spacing space='small' />
+
+        <S.Paragraph>{job.requirements.content}</S.Paragraph>
+
+        <Spacing space='small' />
+
+        <S.List>
+          {job.requirements.items.map((item, index) => (
+            <li key={index}>
+              <S.Paragraph>{item}</S.Paragraph>
+            </li>
+          ))}
+        </S.List>
+
+        <Spacing space='large' />
+
+        <S.Heading>What you will do</S.Heading>
+
+        <Spacing space='small' />
+
+        <S.Paragraph>{job.role.content}</S.Paragraph>
+
+        <Spacing space='small' />
+
+        <S.List as='ol'>
+          {job.role.items.map((item, index) => (
+            <li key={index}>
+              <S.Paragraph>{item}</S.Paragraph>
+            </li>
+          ))}
+        </S.List>
+      </S.JobDetailsContent>
+
+      <Spacing space='large' />
+
+      <S.JobDetailsFooter>
+        <ApplyButton />
+      </S.JobDetailsFooter>
     </Layout>
   );
 }
